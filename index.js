daac4e75102307f037e959201d2cfb48538a90c5
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-07-18 16:10:03 +0800

    fix stuff

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', express.static('public'));
+app.get('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/api/ip', async (req, res) => {
 	const ip = res.socket.remoteAddress;
