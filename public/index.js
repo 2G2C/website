e7803ab3a2e596fdeb3d905a295cbad445c54c85
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-07-19 14:58:37 +0800

    guh

--- public/index.js
+++ public/index.js
@@ -1,8 +1,15 @@
-const ip = fetch("https://localhost:3000/api/ip")
+// massive fucking fail
+// const ip = fetch("https://localhost:3000/api/ip")
+// .then(res => res.json())
+// .then((data) => {
+// 	console.log(data)
+// 	const para = document.createElement("span");
+// 	para.appendChild(document.createTextNode(data.ip))
+// 	document.getElementById("ip_addr").appendChild(para);
+// })
+
+const ip = fetch("ss14://421034.xyz/status")
 .then(res => res.json())
 .then((data) => {
 	console.log(data)
-	const para = document.createElement("span");
-	para.appendChild(document.createTextNode(data.ip))
-	document.getElementById("ip_addr").appendChild(para);
 })
