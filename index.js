75acde1d3cdf19402bf8f2437318f21870fe32a1
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-07-19 15:26:47 +0800

    ogkey done

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
