511192028aa02caa5397ae256fff01c893f7017f
eclips_e <67359748+Just-a-Unity-Dev@users.noreply.github.com>
2022-09-26 13:45:39 +0800

    port 3001

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
