bdc5d703f127f9c0673c7158c27603f2a557fa4c
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-22 21:10:47 +0800

    update website

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors())
 
 app.get('/', express.static(path.join(__dirname, 'public')));
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 })
--- public/index.js
+++ public/index.js
@@ -1,9 +1,1 @@
-// massive fucking fail
-// const ip = fetch("https://localhost:3000/api/ip")
-// .then(res => res.json())
-// .then((data) => {
-// 	console.log(data)
-// 	const para = document.createElement("span");
-// 	para.appendChild(document.createTextNode(data.ip))
-// 	document.getElementById("ip_addr").appendChild(para);
-// })
+console.log(`hi buddy :)`)
